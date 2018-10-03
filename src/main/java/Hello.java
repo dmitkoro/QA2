cc065b2f65deaf2b1c2d0ca0839c0cec3e01afa6
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-03 10:21:20 +0300

    Scaner added

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("!");
-	
 
-    }
+
+
+  Scanner reader = new Scanner(System.in);
+
+                System.out.print("Enter a number: ");
+                int num = reader.nextInt();
+
+                if(num % 2 == 0)
+                    System.out.println(num + " is even");
+                else
+                    System.out.println(num + " is odd");
+            }
+
+
 }
 
